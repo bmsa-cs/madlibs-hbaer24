f56a6663708c6d5a33dd39e42579954fe4e7e685
hbaer24 <73853334+hbaer24@users.noreply.github.com>
2020-11-09 14:04:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,37 @@ Period/Core:
 
 
 """
+
+#Begins game!
+print("Let's play Silly Sentences!")
+import time
+time.sleep(2)
+print("There are a few rules.")
+import time
+time.sleep(1)
+print("...")
+import time
+time.sleep(1)
+print("All verbs MUST be past tense!")
+print("Only appropriate body parts can be chosen.")
+import time
+time.sleep(1)
+print("Enjoy the game!")
+import time
+time.sleep(1)
+print("...")
+import time 
+time.sleep(2)
+
+
+print(input("Enter a name:"))
+import time
+time.sleep(1)
+print(input("Enter a friend name:"))
+import time 
+time.sleep(1)
+
+
+
+
+
