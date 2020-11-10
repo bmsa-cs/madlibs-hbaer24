e7075772daafd28ca478c05bd82736168d651dfc
hbaer24 <73853334+hbaer24@users.noreply.github.com>
2020-11-09 19:53:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,37 +5,58 @@ Period/Core:
 
 
 """
+import time
+from termcolor import colored 
 
 #Begins game!
-print("Let's play Silly Sentences!")
-import time
+print(colored("Let's " + "play" + " Silly Sentences!", 'red'))
 time.sleep(2)
-print("There are a few rules.")
-import time
+print (colored("There are a few rules.", 'yellow'))
 time.sleep(1)
-print("...")
-import time
+print(colored("...", 'green'))
 time.sleep(1)
-print("All verbs MUST be past tense!")
-print("Only appropriate body parts can be chosen.")
-import time
+print(colored("*All verbs MUST be past tense, unless ending in -ing, -ed, etc.*", 'cyan'))
+print(colored("*Only appropriate body parts can be chosen.*",'blue'))
 time.sleep(1)
-print("Enjoy the game!")
-import time
+print(colored("*And lastly, enjoy the game!*",'magenta'))
 time.sleep(1)
-print("...")
-import time 
+print(colored("...", 'red'))
 time.sleep(2)
 
 
-print(input("Enter a name:"))
-import time
-time.sleep(1)
-print(input("Enter a friend name:"))
-import time 
-time.sleep(1)
+person = input("Enter a friend's name:")
+adjectiveone = input("Enter an adjective:")
+place = input("Enter a store/restaurant name:")
+verbone = input("Enter a verb:")
+nounone = input("Enter a noun:")
+verbtwo = input("Enter a verb ending in -ing:")
+bpartone = input("Enter a body part:")
+adjectivetwo = input("Enter an adjective ending in -ly:")
+objectone = input("Enter an object:")
+bparttwo =input("Enter a body part:")
+verbthree = input("Enter a verb:")
+nountwo = input("Enter a noun:")
+family  = input("Enter a person (father, mother, grandparent, etc.):")
+caps = input("Enter a word in ALL CAPS:")
+verbfour = input("Enter a verb:")
+facebody = input("Enter a body part of the face:")
+bpartthree = input("Enter a body part:")
+verbfive = input("Enter a verb:")
+adjectivethree = input("Enter an adjective:")
+emotionone = input("Enter an emotion:")
+adjectivefour = input("Enter an adjective:")
+nounthree = input("Enter a noun:")
+objecttwo = input("Enter an object:")
+objectthree = input("Enter an object:")
+expressionone = input("Enter an expression ending in -ed:")
+adjectivefour = input("Enter an adjective:")
+verbsix = input("Enter a verb ending with -ed:")
+
+print("Great!" + " Now let's move on.")
+print(colored("Generating..."))
+time.sleep(3)
 
 
-
-
-
+print(f"Today I went to the mall. At the mall, I saw {person}.")
+print(f"{person} and I gave each other a {adjectiveone} high-five and walked over to {place}.\n At {place}, {person} almost immediately {verbone} over to the {nounone}. “Wait!” I said, {verbtwo} over to {person}. I grabbed his {bpartone} and pulled him over to the door. “What are you doing?” I said {adjectivetwo}.\n  “  I was looking for a/an {objectone} and I found it,” {person} said, pointing his {bparttwo} to the {objectone}. “O.K. Go get it, then!” I let go of his {bpartone} and he {verbthree} to the {nountwo}. I watched him as he barreled into the {nounone} as he tripped and knocked over a {family}. “{caps}!!!!” The {family} yelled, and whacked Dave with the {objectone}. They had gotten up and left as I {verbfour} to help {person} up. “Oops,” {person} replied, rubbing his {facebody}. I reached out to {person} with my {bpartthree} and he got up.\n  “What is all this?!” The store manager said, who {verbfive} over to us. She pointed her {adjectivethree} finger at me and {person}. “Clean it up! And get out!” She yelled, disturbing all of the customers around us. I felt {emotionone}. {person} looked at me and opened their mouth to speak, but paused as the lady walked {adjectivefour} to us. She picked up the {objectone} that {person}
+wanted! “Hurry it up! And pay for everything you broke!” She swatted her hands around to queue us to begin cleaning the messy floor. I grabbed a {nounthree} and swept with it. {person} picked up a {objecttwo} and a {objectthree} and started scrubbing the floors. After we finished cleaning, we handed over our leftover money we were going to use to buy the {objectone} with. She {expressionone} and pointed her {adjectivefour} finger to the door. I {verbsix} {person} on the back. “We’ll get it Amazon I guess.” I told them. We headed back into the mall and didn’t enter {place} again.
